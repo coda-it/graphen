f07e7e3399dd871c73a613409650001928b2c88c
Oskar Szura <oskarszura@Air-Oskar.lan>
2024-04-29 08:34:58 +0200

    CODA-307 - Remove margin for dropdown without label

--- src/components/Dropdown/integration/dropdown.spec.js
+++ src/components/Dropdown/integration/dropdown.spec.js
@@ -3,7 +3,7 @@ describe("dropdown menu", () => {
     cy.visit("localhost:3000");
     cy.get("[data-cy=dropdown-button]")[0]
       .click()
-      .get("[data-cy=dropdown-item]") 
+      .get("[data-cy=dropdown-item]")
       .contains("Red")
       .click()
       .get("[data-cy=dropdown-button]")
